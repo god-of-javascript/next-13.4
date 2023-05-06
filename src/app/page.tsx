0d48638b1c4216d0623ecadc5c7d0744cf4cc309
The god himself! <124286495+god-of-javascript@users.noreply.github.com>
2023-05-06 10:44:58 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-center p-4">
+      <h1 className="text-center text-3xl md:text-7xl">
+        <span>Next.js Template</span>
+      </h1>
+    </main>
+  );
+}
